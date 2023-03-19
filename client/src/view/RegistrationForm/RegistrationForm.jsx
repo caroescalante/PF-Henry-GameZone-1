b5fa3d8dec7a284054b2746f07eea4ebe5056288
Leonardo Guerrero <leo.19-20@hotmail.com>
2023-03-19 01:33:21 -0500

    registration/template

--- client/src/view/RegistrationForm/RegistrationForm.jsx
+++ client/src/view/RegistrationForm/RegistrationForm.jsx
@@ -39,16 +39,103 @@ function RegistrationForm() {
   return (
 
     <div className={style.user}>
-      {/* <header>Registration</header> */}
+      <div className={style.registration}>
+        <div className={style.container}>
 
-        {/* <form action="#">
-          <div className={style.formFirst}>
+          <header>Registration</header>
 
-          </div>
+            <form action="#">
+              <div className={style.formFirst}>             
+                <div className={style.detailsPersonal}>
 
-        </form> */}
-      
+                  <span className={style.title}>Personal Details</span>
 
+                  <div className={style.fields}>
+
+                  <div>
+                    <label>Names</label>
+                    <input 
+                    type="text" 
+                    placeholder="Enter your name" 
+                    required
+                    ></input>
+                  </div>
+
+                  <div>
+                    <label>Surnames</label>
+                    <input 
+                    type="text" 
+                    placeholder="Enter your surname" 
+                    required></input>
+                  </div>
+
+                  <div>
+                    <label>Image  </label>
+                    <input 
+                    type="text" 
+                    placeholder="Paste link photo URL" 
+                    required></input>
+                  </div>
+
+                  <div>
+                    <label>Phone</label>
+                    <input 
+                    type="text" 
+                    placeholder="Enter your phone" 
+                    required></input>
+                  </div>
+
+                  <div>
+                    <label>Email</label>
+                    <input 
+                    type="text" 
+                    placeholder="Enter your email" 
+                    required></input>
+                  </div>
+
+                  <div>
+                    <label>Estado</label>
+                    <input 
+                    type="email" 
+                    placeholder="Enter your estado" 
+                    required></input>
+                  </div>
+
+                  <div>
+                    <label>Classification</label>
+                    <input 
+                    type="text" 
+                    placeholder="Enter your rol" 
+                    required></input>
+                  </div>
+
+                  <div>
+                    <label>Password</label>
+                    <input 
+                    type="password" 
+                    placeholder="Enter your password" 
+                    required></input>
+                  </div>                
+
+                  </div>
+
+                  <p className={style.note}>
+                    If you are already registered and want to update your data, just fill in the data you want to update.
+                  </p>
+
+                  <div className={style.containerButton}>
+                    <button 
+                    className={style.button}>
+                    Record Data
+                    <ion-icon name="person-add-outline"></ion-icon>
+                    </button>
+                  </div>
+
+                </div>
+              </div>
+            </form>      
+        </div>
+      </div>
     </div>
   )
 }
