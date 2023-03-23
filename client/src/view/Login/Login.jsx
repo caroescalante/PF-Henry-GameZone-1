13f625c69e17987c6f5050a424fc00e0991771f1
Leonardo Guerrero <leo.19-20@hotmail.com>
2023-03-23 10:26:21 -0500

    auth

--- client/src/view/Login/Login.jsx
+++ client/src/view/Login/Login.jsx
@@ -6,6 +6,7 @@ import style from '../Login/Login.module.css';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 import Swal from 'sweetalert2';
+import  GoogleLogin  from  'react-google-login' ;
 
 
 const Login = () => {
@@ -188,6 +189,14 @@ const Login = () => {
             type="submit">
             Log in
             </button>
+
+            <GoogleLogin
+            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+            />,
 
             <div className={style.register}>
 
