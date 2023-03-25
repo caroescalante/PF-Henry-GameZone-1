d550042cd8080a4c94c01e1c64ebfb0e265934a8
PabloAlvarez422 <pabloalvarez4222324@gmail.com>
2023-03-25 15:45:14 -0300

    cambios

--- client/src/view/PaymentFailure/PaymentFailure.jsx
+++ client/src/view/PaymentFailure/PaymentFailure.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import styles from "./PaymentFailure.module.css";
+import { Link } from "react-router-dom";
 
 export default function PaymentFailure (){
     return(
-
-        <div>Payment Failure</div>
+        <div className={styles.Background}>
+            <div>
+            <img src="../../Image/paymentFailure" alt="payment failure"/>
+            </div>
+            
+            <div>
+            <h2>Failed Transaction!</h2>
+            </div>
+            
+            <div>
+            <h3>There is an error on your card.<br/>Your bank has rejected our attempt to authorize your card,<br/> please contact them for more information.</h3>
+            </div>
+            
+            <Link to="/"  >
+                Return to page
+            </Link>
+        </div>
     )
 }
