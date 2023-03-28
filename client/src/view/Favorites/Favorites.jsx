70f6b95cfc69971688c32d0e81478d7ec76f786d
NAHUE86 <nahuelnestorcastelar@gmail.com>
2023-03-27 23:26:17 -0300

    styles favoritos

--- client/src/view/Favorites/Favorites.jsx
+++ client/src/view/Favorites/Favorites.jsx
@@ -17,20 +17,22 @@ const Favorites = () => {
     return (
         
         <div className={style.background}>
+            {favorites.length ? 
+               
             <div className={style.containerAllCards}>
-            {favorites.length ? favorites.map((favorite, index) => {
-                return <div className={style.containerCards}>
-                        <div className={style.favoriteCard} key={index}>
-                         <img src={favorite.image} alt="favorite-image" className={style.favoriteImage} />
-                        <h2 className={style.favoriteName}>{favorite.name}</h2>
-                        <div className={style.favoriteButton}>
-                          <button onClick={() => removeFavHandler(favorite.id)} className={style.trashButton}><i class="fas fa-trash"></i></button>
-                        </div>
-                        </div>
-                </div>          
-                  
-            }) : <h3 className={style.favoriteEmpty}>No games were added to favorites</h3>}
-            </div>  
+               {favorites.map((favorite, index) => { 
+                return  <div className={style.containerCards}>
+                         <div className={style.favoriteCard} key={index}>
+                           <img src={favorite.image} alt="favorite-image" className={style.favoriteImage} />
+                           <h2 className={style.favoriteName}>{favorite.name}</h2>
+                           <button onClick={() => removeFavHandler(favorite.id)} className={style.trashButton}><i class="fas fa-trash"></i></button>
+                         </div>
+                       </div>          
+                }) 
+                }
+            </div> 
+           : (<p className={style.favoriteEmpty}>No games were added to favorites</p>) 
+   }
         </div>
         
     );
