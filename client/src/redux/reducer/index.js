8b02aaceefa23b9c7ab19b415b90b18d36ad213b
PabloAlvarez422 <pabloalvarez4222324@gmail.com>
2023-04-02 20:43:11 -0300

    cambios

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -180,7 +180,7 @@ function rootReducer(state = initialState, action) {
               ...state,
               detail:[],
               allGames:[],
-              emailUser: [],
+              // emailUser: [],
               searchError: null
           }; 
           
