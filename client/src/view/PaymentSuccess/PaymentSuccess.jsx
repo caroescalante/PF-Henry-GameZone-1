1d76b1362eea890076c76f3bc871946d078f52a1
NAHUE86 <nahuelnestorcastelar@gmail.com>
2023-04-04 12:46:47 -0300

    a

--- client/src/view/PaymentSuccess/PaymentSuccess.jsx
+++ client/src/view/PaymentSuccess/PaymentSuccess.jsx
@@ -1,10 +1,47 @@
+// import React, { useEffect } from "react";
+// import styles from "./PaymentSuccess.module.css";
+// import axios from "axios";
+// import { useSelector } from "react-redux"; 
+   
+// function PaymentSuccess() {
+  
+//   const email = useSelector(state => state.emailUser.email);
+  
+//    useEffect(() => {
+//     axios.post("http://localhost:3001/payment/sendEmail", { email: email });
+//   }, []);
+
+//   return (
+//     <div className={styles.Background}>
+//         <div className={styles.containerCard}>
+//           <div className={styles.card}>
+//           <div className={styles.checkIcon}></div>
+//           <h1 className={styles.title}>Your payment has been successfully completed</h1>
+//           <h2 className={styles.text}> We will send you the token number to your email as soon as possible.</h2>
+//           <h3 className={styles.text2}>Thanks for your purchase!</h3>
+//           <a className={styles.button} href="/">Return to page</a>
+//         </div>
+//         </div>
+//     </div>
+//   );
+// };
+
+// export default PaymentSuccess;
+
+
+// const PaymentData = {
+//     client: 'Juan Perez',
+//     amount: 1000,
+//     date: '2023-03-30'
+// };
+
 import React, { useEffect } from "react";
 import styles from "./PaymentSuccess.module.css";
 import axios from "axios";
-import { useSelector } from "react-redux"; 
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom"; 
    
 function PaymentSuccess() {
-  
   const email = useSelector(state => state.emailUser.email);
    useEffect(() => {
     axios.post("http://localhost:3001/payment/sendEmail", { email: email });
@@ -18,7 +55,7 @@ function PaymentSuccess() {
           <h1 className={styles.title}>Your payment has been successfully completed</h1>
           <h2 className={styles.text}> We will send you the token number to your email as soon as possible.</h2>
           <h3 className={styles.text2}>Thanks for your purchase!</h3>
-          <a className={styles.button} href="/">Return to page</a>
+          <Link to="/" className={styles.button}>Return to page</Link>
         </div>
         </div>
     </div>
