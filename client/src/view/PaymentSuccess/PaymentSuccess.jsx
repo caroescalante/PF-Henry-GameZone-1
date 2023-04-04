dc959229b91b95503a5b6a457cc7dbe986a5d3a1
NAHUE86 <nahuelnestorcastelar@gmail.com>
2023-04-04 13:50:52 -0300

    mail ready

--- client/src/view/PaymentSuccess/PaymentSuccess.jsx
+++ client/src/view/PaymentSuccess/PaymentSuccess.jsx
@@ -1,51 +1,16 @@
-// import React, { useEffect } from "react";
-// import styles from "./PaymentSuccess.module.css";
-// import axios from "axios";
-// import { useSelector } from "react-redux"; 
-   
-// function PaymentSuccess() {
-  
-//   const email = useSelector(state => state.emailUser.email);
-  
-//    useEffect(() => {
-//     axios.post("http://localhost:3001/payment/sendEmail", { email: email });
-//   }, []);
-
-//   return (
-//     <div className={styles.Background}>
-//         <div className={styles.containerCard}>
-//           <div className={styles.card}>
-//           <div className={styles.checkIcon}></div>
-//           <h1 className={styles.title}>Your payment has been successfully completed</h1>
-//           <h2 className={styles.text}> We will send you the token number to your email as soon as possible.</h2>
-//           <h3 className={styles.text2}>Thanks for your purchase!</h3>
-//           <a className={styles.button} href="/">Return to page</a>
-//         </div>
-//         </div>
-//     </div>
-//   );
-// };
-
-// export default PaymentSuccess;
-
-
-// const PaymentData = {
-//     client: 'Juan Perez',
-//     amount: 1000,
-//     date: '2023-03-30'
-// };
-
 import React, { useEffect } from "react";
 import styles from "./PaymentSuccess.module.css";
 import axios from "axios";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom"; 
+import { useAuth0 } from "@auth0/auth0-react";
    
 function PaymentSuccess() {
-  const email = useSelector(state => state.emailUser.email);
+
+const { user, isAuthenticated } = useAuth0(); 
+
    useEffect(() => {
-    axios.post("http://localhost:3001/payment/sendEmail", { email: email });
-  }, []);
+    if(isAuthenticated){
+    axios.post("http://localhost:3001/payment/sendEmail", { email: user.email });
+  }}, [isAuthenticated]);
 
   return (
     <div className={styles.Background}>
@@ -55,7 +20,7 @@ function PaymentSuccess() {
           <h1 className={styles.title}>Your payment has been successfully completed</h1>
           <h2 className={styles.text}> We will send you the token number to your email as soon as possible.</h2>
           <h3 className={styles.text2}>Thanks for your purchase!</h3>
-          <Link to="/" className={styles.button}>Return to page</Link>
+          <a className={styles.button} href="/">Return to page</a>
         </div>
         </div>
     </div>
@@ -63,6 +28,8 @@ function PaymentSuccess() {
 };
 
 export default PaymentSuccess;
+
+
 // const PaymentData = {
 //     client: 'Juan Perez',
 //     amount: 1000,
